226d52b8657048ec739098dcd77c806a7bf24224
pariwesh009 <ajay2322020@outlook.com>
2021-04-05 19:14:24 +0530

    added validations and more unit tests

--- order-service/src/main/java/com/example/order/controller/OrderController.java
+++ order-service/src/main/java/com/example/order/controller/OrderController.java
@@ -1,6 +1,9 @@
 package com.example.order.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,17 +18,15 @@ import com.example.order.service.IOrderService;
 @RestController
 public class OrderController {
 	@Autowired
-	IOrderService orderService;
+	IOrderService orderService; //null
 	
 	@GetMapping("/order")
 	String gerOrders() {
 		return "Hello";
 	}
 	@PostMapping("/order")
-	String createOrder(@RequestBody Order order) {
-		System.out.println(order.getName());
-		orderService.createOrder(order);
-		return "post response";
+	String createOrder( @RequestBody @Valid Order order) {
+		return orderService.createOrder(order);
 	}
 	
 	@PutMapping("/order/{id}")
--- order-service/src/main/java/com/example/order/entity/Order.java
+++ order-service/src/main/java/com/example/order/entity/Order.java
@@ -1,8 +1,18 @@
 package com.example.order.entity;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.lang.NonNull;
+
 public class Order {
+	@NotNull  //mandatory
+	@NotBlank
 	private String name;
-	private float price;
+	@NotNull
+	@Digits(fraction = 2, integer=3 )
+	private float price; //mandatory+numeric,  45.56
 	public String getName() {
 		return name;
 	}
@@ -13,6 +23,9 @@ public class Order {
 		return price;
 	}
 	public void setPrice(float price) {
+		if(price<=0 ) {
+			throw new IllegalArgumentException("price cannot be negative");
+		}
 		this.price = price;
 	}
 }
